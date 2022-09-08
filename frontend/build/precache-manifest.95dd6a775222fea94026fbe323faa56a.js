8ea516d6bdedc371ee16ed341414ea376fe2ea3e
deyAbheer001 <110562028+deyAbheer001@users.noreply.github.com>
2022-09-08 18:46:47 +0530

    first commit

---
+++ frontend/build/precache-manifest.95dd6a775222fea94026fbe323faa56a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7cce9d2e4498a45cf9eec64e93ec45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e7ed5c7b5979dbae4a",
+    "url": "/static/css/main.65ccdce3.chunk.css"
+  },
+  {
+    "revision": "45faa751667d2026efc9",
+    "url": "/static/js/2.3263874f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3263874f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e7ed5c7b5979dbae4a",
+    "url": "/static/js/main.eff3907a.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
